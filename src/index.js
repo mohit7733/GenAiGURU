bb338cd3ef112c36a4afc3760b929785082294ac
Raghu <nagasairaghu@gmail.com>
2024-04-05 20:07:05 +0530

    google

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="326427694928-ld6aa1dvtt907t825k8369rk74b7sf3s.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId="80180052683-ep9tpc4tv5iq8jmcvrnufmvddu44hro6.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </React.StrictMode>
